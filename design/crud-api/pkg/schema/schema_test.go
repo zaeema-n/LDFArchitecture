0de7361a5ecc0f082c416d84eea932a859f63d89
Vibhatha Abeykoon <vibhatha@gmail.com>
2025-05-12 11:57:50 +0530

    fix: adding schema test for graph data ?

--- design/crud-api/pkg/schema/schema_test.go
+++ design/crud-api/pkg/schema/schema_test.go
@@ -251,6 +251,398 @@ func TestSchemaGeneration(t *testing.T) {
 				}
 			}`,
 		},
+		"graph_data_with_nodes": {
+			input: `{
+				"attributes": {
+					"nodes": {
+						"person": {
+							"name": "John",
+							"age": 30,
+							"active": true
+						}
+					}
+				}
+			}`,
+			expected: `{
+				"storage_type": "graph",
+				"type_info": {
+					"type": "string"
+				},
+				"fields": {
+					"nodes": {
+						"storage_type": "map",
+						"type_info": {
+							"type": "string"
+						},
+						"properties": {
+							"person": {
+								"storage_type": "map",
+								"type_info": {
+									"type": "string"
+								},
+								"properties": {
+									"name": {
+										"storage_type": "scalar",
+										"type_info": {
+											"type": "string"
+										}
+									},
+									"age": {
+										"storage_type": "scalar",
+										"type_info": {
+											"type": "int"
+										}
+									},
+									"active": {
+										"storage_type": "scalar",
+										"type_info": {
+											"type": "bool"
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}`,
+		},
+		"graph_data_with_edges": {
+			input: `{
+				"attributes": {
+					"edges": {
+						"knows": {
+							"since": "2020-01-01",
+							"strength": 0.8
+						}
+					}
+				}
+			}`,
+			expected: `{
+				"storage_type": "graph",
+				"type_info": {
+					"type": "string"
+				},
+				"fields": {
+					"edges": {
+						"storage_type": "map",
+						"type_info": {
+							"type": "string"
+						},
+						"properties": {
+							"knows": {
+								"storage_type": "map",
+								"type_info": {
+									"type": "string"
+								},
+								"properties": {
+									"since": {
+										"storage_type": "scalar",
+										"type_info": {
+											"type": "date"
+										}
+									},
+									"strength": {
+										"storage_type": "scalar",
+										"type_info": {
+											"type": "float"
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}`,
+		},
+		"graph_data_with_both": {
+			input: `{
+				"attributes": {
+					"nodes": {
+						"person": {
+							"name": "John",
+							"age": 30
+						}
+					},
+					"edges": {
+						"knows": {
+							"since": "2020-01-01"
+						}
+					}
+				}
+			}`,
+			expected: `{
+				"storage_type": "graph",
+				"type_info": {
+					"type": "string"
+				},
+				"fields": {
+					"nodes": {
+						"storage_type": "map",
+						"type_info": {
+							"type": "string"
+						},
+						"properties": {
+							"person": {
+								"storage_type": "map",
+								"type_info": {
+									"type": "string"
+								},
+								"properties": {
+									"name": {
+										"storage_type": "scalar",
+										"type_info": {
+											"type": "string"
+										}
+									},
+									"age": {
+										"storage_type": "scalar",
+										"type_info": {
+											"type": "int"
+										}
+									}
+								}
+							}
+						}
+					},
+					"edges": {
+						"storage_type": "map",
+						"type_info": {
+							"type": "string"
+						},
+						"properties": {
+							"knows": {
+								"storage_type": "map",
+								"type_info": {
+									"type": "string"
+								},
+								"properties": {
+									"since": {
+										"storage_type": "scalar",
+										"type_info": {
+											"type": "date"
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}`,
+		},
+		"social_network": {
+			input: `{
+				"attributes": {
+					"nodes": [
+						{"id": "user1", "type": "user", "properties": {"name": "Alice", "age": 30, "location": "NY"}},
+						{"id": "user2", "type": "user", "properties": {"name": "Bob", "age": 25, "location": "SF"}},
+						{"id": "user3", "type": "user", "properties": {"name": "Charlie", "age": 35, "location": "LA"}},
+						{"id": "post1", "type": "post", "properties": {"title": "Hello", "content": "World", "created": "2024-03-20"}},
+						{"id": "post2", "type": "post", "properties": {"title": "Graph", "content": "DB", "created": "2024-03-21"}}
+					],
+					"edges": [
+						{"source": "user1", "target": "user2", "type": "follows", "properties": {"since": "2024-01-01"}},
+						{"source": "user2", "target": "user3", "type": "follows", "properties": {"since": "2024-02-01"}},
+						{"source": "user1", "target": "post1", "type": "created", "properties": {"timestamp": "2024-03-20T10:00:00Z"}},
+						{"source": "user2", "target": "post1", "type": "likes", "properties": {"timestamp": "2024-03-20T11:00:00Z"}},
+						{"source": "user3", "target": "post2", "type": "created", "properties": {"timestamp": "2024-03-21T09:00:00Z"}}
+					]
+				}
+			}`,
+			expected: `{
+				"storage_type": "graph",
+				"type_info": {
+					"type": "string"
+				},
+				"fields": {
+					"nodes": {
+						"storage_type": "map",
+						"type_info": {
+							"type": "string"
+						},
+						"properties": {
+							"user": {
+								"storage_type": "map",
+								"type_info": {
+									"type": "string"
+								},
+								"properties": {
+									"name": {
+										"storage_type": "scalar",
+										"type_info": {
+											"type": "string"
+										}
+									},
+									"age": {
+										"storage_type": "scalar",
+										"type_info": {
+											"type": "int"
+										}
+									},
+									"location": {
+										"storage_type": "scalar",
+										"type_info": {
+											"type": "string"
+										}
+									}
+								}
+							},
+							"post": {
+								"storage_type": "map",
+								"type_info": {
+									"type": "string"
+								},
+								"properties": {
+									"title": {
+										"storage_type": "scalar",
+										"type_info": {
+											"type": "string"
+										}
+									},
+									"content": {
+										"storage_type": "scalar",
+										"type_info": {
+											"type": "string"
+										}
+									},
+									"created": {
+										"storage_type": "scalar",
+										"type_info": {
+											"type": "date"
+										}
+									}
+								}
+							}
+						}
+					},
+					"edges": {
+						"storage_type": "map",
+						"type_info": {
+							"type": "string"
+						},
+						"properties": {
+							"follows": {
+								"storage_type": "map",
+								"type_info": {
+									"type": "string"
+								},
+								"properties": {
+									"since": {
+										"storage_type": "scalar",
+										"type_info": {
+											"type": "date"
+										}
+									}
+								}
+							},
+							"created": {
+								"storage_type": "map",
+								"type_info": {
+									"type": "string"
+								},
+								"properties": {
+									"timestamp": {
+										"storage_type": "scalar",
+										"type_info": {
+											"type": "datetime"
+										}
+									}
+								}
+							},
+							"likes": {
+								"storage_type": "map",
+								"type_info": {
+									"type": "string"
+								},
+								"properties": {
+									"timestamp": {
+										"storage_type": "scalar",
+										"type_info": {
+											"type": "datetime"
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}`,
+		},
+		"dependency_graph": {
+			input: `{
+				"attributes": {
+					"nodes": [
+						{"id": "pkg1", "type": "package", "properties": {"name": "core", "version": "1.0.0"}},
+						{"id": "pkg2", "type": "package", "properties": {"name": "utils", "version": "2.1.0"}},
+						{"id": "pkg3", "type": "package", "properties": {"name": "network", "version": "1.5.0"}},
+						{"id": "pkg4", "type": "package", "properties": {"name": "database", "version": "3.0.0"}}
+					],
+					"edges": [
+						{"source": "pkg2", "target": "pkg1", "type": "depends_on", "properties": {"version": ">=1.0.0"}},
+						{"source": "pkg3", "target": "pkg1", "type": "depends_on", "properties": {"version": ">=1.0.0"}},
+						{"source": "pkg4", "target": "pkg2", "type": "depends_on", "properties": {"version": ">=2.0.0"}},
+						{"source": "pkg4", "target": "pkg3", "type": "depends_on", "properties": {"version": ">=1.5.0"}}
+					]
+				}
+			}`,
+			expected: `{
+				"storage_type": "graph",
+				"type_info": {
+					"type": "string"
+				},
+				"fields": {
+					"nodes": {
+						"storage_type": "map",
+						"type_info": {
+							"type": "string"
+						},
+						"properties": {
+							"package": {
+								"storage_type": "map",
+								"type_info": {
+									"type": "string"
+								},
+								"properties": {
+									"name": {
+										"storage_type": "scalar",
+										"type_info": {
+											"type": "string"
+										}
+									},
+									"version": {
+										"storage_type": "scalar",
+										"type_info": {
+											"type": "string"
+										}
+									}
+								}
+							}
+						}
+					},
+					"edges": {
+						"storage_type": "map",
+						"type_info": {
+							"type": "string"
+						},
+						"properties": {
+							"depends_on": {
+								"storage_type": "map",
+								"type_info": {
+									"type": "string"
+								},
+								"properties": {
+									"version": {
+										"storage_type": "scalar",
+										"type_info": {
+											"type": "string"
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}`,
+		},
 	}
 
 	generator := NewSchemaGenerator()
